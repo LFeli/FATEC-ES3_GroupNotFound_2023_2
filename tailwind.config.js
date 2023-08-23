17c88fb61a2c20d57f3e69c333f1c94c983f1fdd
Lucas Felipe <lucasfelipemk@gmail.com>
2023-08-23 20:19:43 -0300

    feat: add releway font in tailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        'raleway':['Raleway', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
