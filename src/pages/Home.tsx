9f3938dcdad898e0d74b97c913b7b0f9afd344c3
Lucas Felipe <lucasfelipemk@gmail.com>
2023-10-05 17:54:51 -0300

    feat: passing link for buttons and adding icons for user submenu

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Button } from "@/components/ui/button";
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from "@/components/ui/navigation-menu";
+import { Button } from "@/components/ui/button";
 import { useEffect, useState } from "react";
+import { BsBoxArrowRight, BsFillHouseFill, BsFillPersonFill, BsGiftFill } from 'react-icons/bs'
+import { FaPaw } from "react-icons/fa";
 
 
 export function Home() {
@@ -88,7 +90,7 @@ export function Home() {
           </NavigationMenuList>
 
           <NavigationMenuItem className="list-none relative">
-            <NavigationMenuTrigger className="bg-transparent p-0 w-fit">
+            <NavigationMenuTrigger className="bg-transparent px-2 w-fit text-white">
               <Avatar className="mr-3 h-8 w-8">
                 <AvatarImage src='src/assets/Bianca.jpg'/>
                 <AvatarFallback>{userAbbreviation}</AvatarFallback>
@@ -97,11 +99,43 @@ export function Home() {
             </NavigationMenuTrigger>
             
             <NavigationMenuContent className="p-4 ">
-              <ul className="flex flex-col gap-3 w-40">
-                <NavigationMenuLink href="#">Link</NavigationMenuLink>
-                <NavigationMenuLink href="#">Link</NavigationMenuLink>
-                <NavigationMenuLink href="#">Link</NavigationMenuLink>
-                <NavigationMenuLink href="#">Link</NavigationMenuLink>
+              <ul className="flex flex-col gap-3 w-48">
+
+                <NavigationMenuLink href="#">
+                  <Button variant={"ghost"} className="w-full flex items-center justify-start gap-4 hover:bg-[#7092BF]/25">
+                    <BsFillHouseFill className="w-4 h-4" />
+                    Home
+                  </Button>
+                </NavigationMenuLink>
+                
+                <NavigationMenuLink href="#">
+                  <Button variant={"ghost"} className="w-full flex items-center justify-start gap-4 hover:bg-[#7092BF]/25">
+                    <BsFillPersonFill className="w-4 h-4" />
+                    Meu perfil
+                  </Button>
+                </NavigationMenuLink>
+
+                <NavigationMenuLink href="#">
+                  <Button variant={"ghost"} className="w-full flex items-center justify-start gap-4 hover:bg-[#7092BF]/25">
+                    <FaPaw className="w-4 h-4" />
+                    Meus pets
+                  </Button>
+                </NavigationMenuLink>
+
+                <NavigationMenuLink href="#">
+                  <Button variant={"ghost"} className="w-full flex items-center justify-start gap-4 hover:bg-[#7092BF]/25">
+                    <BsGiftFill className="w-4 h-4" />
+                    Minhas doações
+                  </Button>
+                </NavigationMenuLink>
+
+                <NavigationMenuLink href="#">
+                  <Button variant={"ghost"} className="w-full flex items-center justify-start gap-4 text-red-600 hover:bg-red-600/10 hover:text-red-600 focus:bg-red-600/10">
+                    <BsBoxArrowRight className="w-4 h-4" />
+                    Sair
+                  </Button>
+                </NavigationMenuLink>
+                
               </ul>
             </NavigationMenuContent>
           </NavigationMenuItem>
@@ -116,7 +150,7 @@ export function Home() {
               {/* left side */}
               <div className="flex flex-col gap-8 w-full max-w-[475px] ">
                 <h1 className="text-white text-6xl font-medium leading-tight">Todo carinho que o seu pet precisa</h1>
-                <p className="text-[#F8F8F8] text-2xl leading-relaxed">Descubra o Oh My Dog, o petshop que oferece agendamentos de serviços como banho, tosa, spa e mais. Agende agora mesmo!</p>
+                <p className="text-[#F8F8F8] text-2xl leading-relaxed">Descubra o Oh My Dog, o pet-shop que oferece agendamentos de serviços como banho, tosa, spa e mais. Agende agora mesmo!</p>
 
                 <Button variant={"outline"} className="w-full max-w-[240px] py-5 text-[#F8F8F8] rounded-full">
                   Veja mais
@@ -131,25 +165,6 @@ export function Home() {
             <div className="absolute top-0 left-0 h-[140%] w-full bg-[#7092BF]" />
           </div>
         </section>
-
-
-
-          {/* Hero */}
-          <section className="relative">
-            <div className="flex pt-14">
-              <div className="">
-                {/* left side */}
-                <div className="">
-
-                </div>
-
-                {/* right side */}
-              </div>
-
-            </div>
-
-            <div className=""></div>
-          </section>
 
       </main>
     </>
