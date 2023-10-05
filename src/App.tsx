1e8a94274ad3790fdfd5cdf4243677506efb1e6d
Lucas Felipe <lucasfelipemk@gmail.com>
2023-10-05 10:28:02 -0300

    feat: import Home page for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { Home } from "./pages/Home";
+
 export function App() {
   return (
-    <div className="flex items-center justify-center h-screen bg-zinc-950">
-      <h1 className="text-5xl font-semibold text-slate-300">Hello World</h1>
-    </div>
+    <Home />
   )
 }
