997ac8568a4d6fc645823e048baaa2c8ccd9152a
Lucas Felipe <lucasfelipemk@gmail.com>
2023-10-07 23:31:19 -0300

    feat: partial of login page done without form section, and adding animals folder in assets

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,36 @@
+import { Button } from "@/components/ui/button";
+
 export function Login() {
   return (
     <>
-      <h1 className="flex items-center justify-center text-2xl h-screen font-bold">Hello World</h1>
+      <header>
+        <Button variant={"ghost"} className="absolute z-10 right-8 top-8 hover:bg-transparent  px-6 py-8">
+          <a href="#" className="">
+            <img src="logo.svg" alt="" />
+          </a>
+        </Button>
+      </header>
+
+
+        {/* Grid box */}
+      <div className="grid grid-cols-2 h-screen w-full bg-[#F2F2F2]">
+      
+        {/* left side */}
+        <div>
+          <main>
+
+          </main>
+
+          <footer className='flex items-center justify-center'>
+            <span className='text-sm absolute bottom-4'>Oh my dog 2023</span>
+          </footer>
+        </div>
+
+        {/* Right side */}
+        <div>
+          <img src="src/assets/animals/login.jpg" alt="" className="h-screen w-full object-cover"/>
+        </div>
+      </div>
     </>
   )
 }
